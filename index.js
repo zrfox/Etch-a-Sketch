99d8641747b0fb2405243ee485ee49784c05992b
Zachary Fox <zrfox97@gmail.com>
2023-01-15 14:02:12 -0800

    Add grid code

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const container = document.querySelector('#container');
+function grid(el) {
+    var container = document.createElement("div");
+    container.id = "main";
+    container.className = "container";
 
+    for (i=0; i<16; i+=1) {
+        var row = document.createElement("div");
+        row.className = "row";
+        row.id = "row" + i;
+      
+        for (k=0; k<16; k+=1) {
+            var box = document.createElement("div"); 
+            box.className = "box";
+            row.appendChild(box);
+        };
+      
+        container.appendChild(row);      
+    };
+  
+    el.appendChild(container);
+};
+
+grid(document.body);

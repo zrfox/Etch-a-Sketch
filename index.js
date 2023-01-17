35343fcf1b9592954cfc2c979efa85738995fcf8
Zachary Fox <zrfox97@gmail.com>
2023-01-16 16:52:25 -0800

    Test different grid code

--- index.js
+++ index.js
@@ -1,23 +1,78 @@
-function grid(el) {
-    var container = document.createElement("div");
-    container.id = "main";
-    container.className = "container";
+/*
+    var column = 16;
+    var row = 16;
 
-    for (i=0; i<16; i+=1) {
-        var row = document.createElement("div");
-        row.className = "row";
-        row.id = "row" + i;
-      
-        for (k=0; k<16; k+=1) {
-            var box = document.createElement("div"); 
-            box.className = "box";
-            row.appendChild(box);
-        };
-      
-        container.appendChild(row);      
-    };
-  
-    el.appendChild(container);
-};
+    function makeGrid(columns, rows){
+    var grid = document.createElement('div');
+    grid.className = 'grid';
+    for (var i = 0; i < columns; ++i) {
+        var column = document.createElement('div'); // create column
+        column.className = 'column';
+        for (var j = 0; j < rows; ++j) {
+            var row = document.createElement('div'); // create row
+            row.className = 'row';
+            column.appendChild(row); // append row in column
+        }
+        grid.appendChild(column); // append column inside grid
+    }
+    document.body.appendChild(grid);
+}
 
-grid(document.body);
+function changeGridSize(size){
+    column=size;
+    row=size;
+}
+
+changeGridSize(4);
+makeGrid(column,row);
+*/
+const container = document.querySelector('.container');
+const sizeEl = document.querySelector('.size');
+let size = sizeEl.value;
+const color= document.querySelector('.color');
+const resetBtn = document.querySelector('.btn');
+
+let draw = false;
+
+function populate(size){
+    container.style.setProperty('--size', size);
+    for (let i = 0;i< size * size; i++) {
+    const div = document.createElement('div');
+    div.classList.add('pixel');
+
+    div.addEventListener('mouseover',function(){
+        if(!draw)return;
+        div.style.backgroundColor = color.value;
+    })
+    div.addEventListener('mousedown',function(){
+        div.style.backgroundColor = color.value;
+    })
+    container.appendChild(div);
+    }
+}
+
+
+
+window.addEventListener("mousedown",function(){
+    draw = true;
+})
+window.addEventListener("mouseup",function(){
+    draw = false;
+})
+populate(size);
+
+function reset (){
+    container.innerHTML = '';
+    populate(size);
+}
+
+resetBtn.addEventListener('click', reset);
+
+sizeEl.addEventListener('change', function(){
+   if(sizeEl.value > 100)return;
+
+    size = sizeEl.value;
+    reset();
+})
+
+populate(size);
